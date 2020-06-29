fc7a17d41825c992d57556b02f607a1633f6cd39
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-29 20:26:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ const playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return playlist[artistName] = songTitle
+  playlist[artistName] = songTitle
 }
 
 function removeFromPlaylist(playlist, artistName){
